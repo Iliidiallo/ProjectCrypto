8baf4f5b631006bd604da7f2b7081ce53c3dc853
serigne.sylla <mor@DESKTOP-9MJ0FN1.localdomain>
2023-03-29 00:06:04 +0200

    implementation des 4 premieres etapes

--- src/CryptoSolitaire/Main.java
+++ src/CryptoSolitaire/Main.java
@@ -7,10 +7,17 @@ public class Main {
     public static void main(String[] args) {
         Carte carte = new Carte();
         carte.initCarteAndMapBridge();
-       // carte.reculJokerRouge();
+        for (int i = 0; i < 20; i++) {
+            carte.reculJokerRouge();
+        }
+
+
+        // carte.doubleCoupeParRapportAuxJokers();
        // carte.reculJokerRouge();
         //carte.reculJokerNoir();
         //carte.reculJokerNoir();
+
+        carte.coupeSimpleSelonDerniereCarte();
 
     }
 }
--- src/modele/Carte.java
+++ src/modele/Carte.java
@@ -28,8 +28,7 @@ public class Carte {
         // Ajout des jokers
         cartesNumerosBridgeMap.put("joker_noir", 53);
         paquet.add("joker_noir");
-        cartesNumerosBridgeMap.put("joker_rouge", 54);
-
+        cartesNumerosBridgeMap.put("joker_rouge", 53);
         paquet.add("joker_rouge");
     }
     public void reculJokerNoir(){
@@ -45,7 +44,6 @@ public class Carte {
         System.out.println(paquet);
     }
     public void reculJokerRouge(){
-        System.out.println(paquet);
         //On fait reculer le joker rouge de deux positions
         int indexJokerRouge = paquet.indexOf("joker_rouge");
         if (indexJokerRouge == 53){
@@ -61,42 +59,49 @@ public class Carte {
     }
 
     public void doubleCoupeParRapportAuxJokers(){
+        System.out.println("avant coupe");
+        System.out.println(paquet);
         int indexJokerRouge = paquet.indexOf("joker_rouge");
         int indexJokerNoir = paquet.indexOf("joker_noir");
+        ArrayList<String> borneInf;
+        ArrayList<String> borneSup;
+        ArrayList<String> borneCent;
         if (indexJokerRouge<indexJokerNoir){
-            ArrayList<String> borneInf = new ArrayList(paquet.subList(0, indexJokerRouge-1));
-            ArrayList<String> borneSup = new ArrayList(paquet.subList(indexJokerNoir+1, 53));
-            ArrayList<String> borneCent = new ArrayList(paquet.subList(indexJokerRouge,indexJokerNoir));
-            // On insert ce qui vient avant le joker rouge dans le paquet
-            for (int i = 0; i < borneInf.size(); i++) {
-                paquet.set(i, borneInf.get(i));
-            }
-            // On insert ce qui vient après le joker noir dans le paquet
-            for (int i = 0; i < borneCent.size(); i++) {
-                paquet.set(i+borneInf.size(), borneCent.get(i));
-            }
-            for (int i = 0; i < borneSup.size(); i++) {
-                paquet.set(i+borneInf.size()+borneCent.size(), borneSup.get(i));
-            }
-
+            borneInf = new ArrayList(paquet.subList(0, indexJokerRouge));
+            borneSup = indexJokerNoir==53?new ArrayList():new ArrayList(paquet.subList(indexJokerNoir+1, 53));
+            borneCent = new ArrayList(paquet.subList(indexJokerRouge,indexJokerNoir+1));
         }
         else{
-            ArrayList<String> borneInf = new ArrayList(paquet.subList(0, indexJokerNoir-1));
-            ArrayList<String> borneSup = new ArrayList(paquet.subList(indexJokerRouge+1, 53));
-            ArrayList<String> borneCent = new ArrayList(paquet.subList(indexJokerNoir,indexJokerRouge));
-            // On insert ce qui vient avant le joker noir dans le paquet
-            for (int i = 0; i < borneInf.size(); i++) {
-                paquet.set(i, borneInf.get(i));
-            }
-            // On insert ce qui vient après le joker rouge dans le paquet
-            for (int i = 0; i < borneCent.size(); i++) {
-                paquet.set(i+borneInf.size(), borneCent.get(i));
-            }
-            for (int i = 0; i < borneSup.size(); i++) {
-                paquet.set(i+borneInf.size()+borneCent.size(), borneSup.get(i));
-            }
+            borneInf = new ArrayList(paquet.subList(0, indexJokerNoir));
+            borneSup = indexJokerRouge==53?new ArrayList():new ArrayList(paquet.subList(indexJokerRouge+1, 53));
+            borneCent = new ArrayList(paquet.subList(indexJokerNoir,indexJokerRouge+1));
         }
+        for (int i = 0; i < borneSup.size(); i++) {
+            paquet.set(i, borneSup.get(i));
+        }
+        for (int i = 0; i < borneCent.size(); i++) {
+            paquet.set(i+borneSup.size(), borneCent.get(i));
+        }
+        for (int i = 0; i < borneInf.size(); i++) {
+            paquet.set(i+borneSup.size()+borneCent.size(), borneInf.get(i));
+        }
+        System.out.println("après coupe");
+        System.out.println(paquet);
+    }
 
+    public void coupeSimpleSelonDerniereCarte(){
+        System.out.println(paquet);
+        int numeroDerniereCarte = cartesNumerosBridgeMap.get(paquet.get(53));
+        System.out.println(numeroDerniereCarte);
+        ArrayList<String> borneInf = new ArrayList(paquet.subList(0, numeroDerniereCarte));
+        ArrayList<String> borneSup = new ArrayList(paquet.subList(numeroDerniereCarte, 53));
+        for (int i = 0; i < borneSup.size(); i++) {
+            paquet.set(i, borneSup.get(i));
+        }
+        for (int i = 0; i < borneInf.size(); i++) {
+            paquet.set(i+borneSup.size(), borneInf.get(i));
+        }
+        System.out.println(paquet);
     }
 
     public int getNumeroCarte(String carte) {
