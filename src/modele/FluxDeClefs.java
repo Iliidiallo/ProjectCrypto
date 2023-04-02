1b576c287b64cbfd94a3b210d9cca299f27a5480
diallo <Iliassou_Diallo@etu.u-bourgogne.fr>
2023-04-02 18:57:13 +0200

    implementation de la methode decrypter message

--- src/modele/FluxDeClefs.java
+++ src/modele/FluxDeClefs.java
@@ -20,7 +20,25 @@ public class FluxDeClefs {
         }
         return messageCrypte;
     }
-
-
+public String decrypterMessage(String message, Carte carte) {
+        String messageDecrypte = "";
+        for (int i = 0; i < message.length(); i++) {
+            char lettre = message.charAt(i);
+            int numeroLettre = (int) lettre;
+            int code = carte.lectureLettrePseudoAleatoire();
+            System.out.println((char)(code+96));
+            int numeroLettreDecryptee = numeroLettre - code;
+            char lettreDecryptee;
+            if (Character.isUpperCase(lettre)) {
+                lettreDecryptee = (char) (numeroLettreDecryptee < 'A' ? numeroLettreDecryptee + 26 : numeroLettreDecryptee);
+            } else {
+                lettreDecryptee = (char) (numeroLettreDecryptee < 'a' ? numeroLettreDecryptee + 26 : numeroLettreDecryptee);
+            }
+            messageDecrypte = messageDecrypte + lettreDecryptee;
+        }
+        return messageDecrypte;
+    }
 }
+
+
 
